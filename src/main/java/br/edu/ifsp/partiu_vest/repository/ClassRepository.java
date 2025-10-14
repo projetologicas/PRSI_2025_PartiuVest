3112cfb27c2f7cdd59604b97fdd809cf88ce8533
guilhermefragiacomo <guilhermefragiacomo1@gmail.com>
2025-10-14 09:46:22 -0300

    test

--- src/main/java/br/edu/ifsp/partiu_vest/repository/ClassRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifsp.partiu_vest.repository;
-
-import br.edu.ifsp.partiu_vest.model.Class;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClassRepository extends JpaRepository<Class, Long> {}
-
