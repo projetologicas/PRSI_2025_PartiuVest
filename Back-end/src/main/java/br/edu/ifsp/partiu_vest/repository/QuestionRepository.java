bef78b08cb0e75bc9b0563e8ff720947432d874b
Rauan Caracciolo <rauancaracciolo@gmail.com>
2025-11-07 13:57:56 -0300

    Import correction

--- Back-end/src/main/java/br/edu/ifsp/partiu_vest/repository/QuestionRepository.java
+++ Back-end/src/main/java/br/edu/ifsp/partiu_vest/repository/QuestionRepository.java
@@ -2,6 +2,8 @@ package br.edu.ifsp.partiu_vest.repository;
 
 import br.edu.ifsp.partiu_vest.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {}
 
